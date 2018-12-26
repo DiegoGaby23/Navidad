18fae48ceb05cec8c27f21ac510da02e755e18a8
DiegoGaby <gabyperezmota@icloud.com>
2018-12-26 01:17:16 +0100

    J'utilise un controller pour bien organiser mon code

---
+++ etages.js
@@ -0,0 +1,45 @@
+// J'utilise un controller pour bien organiser mon code
+
+const { etages } = require('../db.json');
+
+module.exports = {
+    getEtages: (req, res) => {
+        res.json({ etages })
+    },
+
+
+    addEtages: (req, res) => {
+        const { name } = req.body;
+        etages.push({
+            name,
+            id: etages.length
+        })
+
+        // J'envoie un message un Json qui me permet de savoir
+        // que les informations ont bien étaient enregistrer
+        res.json({
+            'Succes': true,
+            'msg': 'Successfully added'
+        });
+    },
+
+    updateEtages: (req, res) => {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        etages.forEach((etages, i) => {
+            if (etages.id === Number(id)) {
+                etages.name = name;
+            }
+        });
+
+        res.json({
+            'Succes': true,
+            'msg': 'Updated'
+        });
+    },
+
+    deleteEtages: (req, res) => {
+
+    }
+};
